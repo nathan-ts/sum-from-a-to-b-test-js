e0f56477a6241ab37bcaea4130f82142cf1c6341
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-11 20:57:08 +0000

    complete

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN > toN) {
+    let max = fromN;
+    fromN = toN;
+    toN = max;
+  }
+  if (fromN === toN) return toN;
+  else return fromN + sum(fromN + 1, toN);
 }
 
 module.exports = sum;
